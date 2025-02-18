28a04e29f5664387c1e830233236c889f402ca8c
Ben Cottrell <bencot@windowslive.com>
2025-02-18 13:33:55 +1300

    Made cards spritesheet transparent

--- create_spritesheet.sh
+++ create_spritesheet.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-cd assets
-montage `ls cards/*.svg` -tile 6x9 -geometry 120x180 -density 288 -filter Lanczos spritesheet.png
+pushd assets
+#montage `ls cards/*.svg` -tile 6x9 -geometry 120x180 -density 288 -filter Lanczos spritesheet.png
+montage -background none `ls cards/*.svg` -tile 6x9 -geometry 120x180+8+8 -density 288 -filter Lanczos spritesheet-a.png
+popd
